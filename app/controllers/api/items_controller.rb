c6f01e36e1bce600a119654362beab74fb36e874
Emily Pena <emilyapena@gmail.com>
2020-04-13 10:24:33 -0600

    Rails controller

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,2 +1,19 @@
 class Api::ItemsController < ApplicationController
+  def index
+    render json: Item.all
+  end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      render json: item
+    else
+      render json: { errors: item.errors }, status :unprocessable_entity
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :image, :description, :image)
+  end
 end
