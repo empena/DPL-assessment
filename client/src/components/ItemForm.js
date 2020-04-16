e082519b3f9c4f83ee4ed3a5eb42412138714afd
Emily Pena <emilyapena@gmail.com>
2020-04-15 21:57:03 -0600

    styled ItemForm

--- client/src/components/ItemForm.js
+++ client/src/components/ItemForm.js
@@ -15,24 +15,33 @@ const ItemForm = (props) => {
     })
   }
 
+  const handleClick = () => {
+    return 
+  }
+
   return (
-    <form onSubmit={handleSubmit}>
+   <div style={styles.popup}>
+     <div style={styles.popupContent}>
+     <span style={styles.close} onClick={handleClick}>&times;</span>
+     <form onSubmit={handleSubmit} style={styles.form}>
       <label>
-        Name
+        Name 
         <input
         type="string"
         required
         name="name"
         onChange={(e) => setName(e.target.value)} />
       </label>
+      <br/>
       <label>
-        Description
+        Description 
         <input
         type="text"
         required
         name="description"
         onChange={(e) => setDescription(e.target.value)} />
       </label>
+      <br/>
       <label>
         Image
         <input
@@ -41,6 +50,7 @@ const ItemForm = (props) => {
         name="image"
         onChange={(e) => setImage(e.target.value)} />
       </label>
+      <br/>
       <label>
         Likes
         <input
@@ -49,9 +59,52 @@ const ItemForm = (props) => {
         name="likes"
         onChange={(e) => setLikes(e.target.value)} />
       </label>
-      <input type='submit' value='Submit' />
+      <br/>
+      <input style={styles.button} type='submit' value='Submit' />
     </form>
-  )
-}
+    </div>
+   </div>
+  );
+ }
 
-export default ItemForm
+ export default ItemForm
+
+ const styles = {
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignContent: 'flex-start',
+    color: 'black',
+  },
+  button: {
+    margin: '1em',
+    padding: '1em',
+    height: 'auto',
+    width: '15%',
+    borderRadius: '.25em',
+    fontSize: '.75em',
+  },
+ popup: {
+    position: 'fixed',
+    zIndex: '1',
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(0, 0, 0, .5)',
+  },
+  popupContent: {
+    backgroundColor: 'white',
+    position: 'absolute',
+    top: '20%',
+    left: '30%',
+    width: '40%',
+    padding: '20px',
+    borderRadius: '5px',
+    border: '2px solid black',
+  },
+  close: {
+    color: 'Black',
+    float: 'right',
+    cursor: 'pointer',
+  },
+}
